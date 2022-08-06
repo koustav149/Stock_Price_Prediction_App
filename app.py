d7b77d3a45de9982a9203c974cb5b2085424dbfd
Koustav Ghosh <koustav149@gmail.com>
2022-08-07 01:19:54 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
+import pandas as pd
+
+stocks_df = pd.read_csv("nasdaq_screener_1659813047862.csv")
+
+stocks_symbol = stocks_df.Symbol.tolist()
+START = "2016-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Forecast App')
+
+
+selected_stock = st.selectbox('Select stock for prediction', stocks_symbol)
+
+n_years = st.slider('Years of prediction:', 1, 5)
+period = n_years * 365
+
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader(stocks_df[stocks_df.Symbol == selected_stock].Name.values[0])
+st.write(data.tail())
+
+
+# Plot raw data
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.header('Forecast data')
+st.write(forecast.tail())
+
+st.subheader(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.subheader("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
---
+++ main.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_ployly
+from plotly import graph_objs as go
+
+Start
